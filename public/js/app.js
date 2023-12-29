d892aa2890b363bab1a63bf48e664d1fafa64cf0
ttchien <chientt.00.student@fit.tdc.edu.vn>
2023-12-29 21:02:14 +0700

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -13,30 +13,3 @@ if (signinModal) {
     event.relatedTarget;
   });
 };
-
-$(".related__products__slider").owlCarousel({
-  loop: true,
-  margin: 0,
-  items: 4,
-  dots: false,
-  nav: true,
-  navText: ["<span class='arrow_carrot-left'><span/>", "<span class='arrow_carrot-right'><span/>"],
-  smartSpeed: 1200,
-  autoHeight: false,
-  autoplay: true,
-  responsive: {
-      0: {
-          items: 1
-      },
-      480: {
-          items: 2
-      },
-      768: {
-          items: 3
-      },
-      992: {
-          items: 4
-      },
-  }
-});
-
